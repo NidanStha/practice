d4f6529009e91629f1f8500e6ba92be62d53cbbc
NidanStha <nidanshrestha@gmail.com>
2020-01-08 17:34:04 +0545

    nothing

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css" />
     <title>PHP practice</title>
-    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <div class="">
+    <div class="session1">
       <form class="" action="index.php" method="post" autocomplete="off">
         <label for="num_value">Number: </label>
         <input type="text" id="num_value" name="mult" value="" required/>
