f901908d954fb72a764f4a94017fa1c8fc8320d4
NidanStha <nidanshrestha@gmail.com>
2020-01-21 21:59:24 +0545

    php database insert and select

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>PHP practice</title>
   </head>
   <body>
-    <nav>
+    <!-- <nav>
       <ol>
         <a href="#session1">
           <li>Multiplication</li>
@@ -24,7 +24,7 @@
           <li></li>
         </a>
       </ol>
-    </nav>
+    </nav> -->
 
     <div class="session1">
       <h5>Multiplication</h5>
@@ -123,6 +123,59 @@
         $book1->display();
 
       ?>
+    </div>
+
+    <div class="session6">
+      <h5>database</h5>
+
+      <?php
+        $host="localhost";
+        $user="root";
+        $pass="";
+        $db="continue";
+        $con = mysqli_connect($host,$user,$pass,$db);
+      ?>
+
+      <form action="index.php" method="post">
+        <label for="nmn">Name: </label><input type="text" name="iname" id="nmn" required autocomplete="off"><br />
+        <label for="pho">Phone: </label><input type="text" name="iphone" id="pho" required autocomplete="off"><br />
+        <input type="submit" value="submit" name="submit">
+      </form>
+
+      <?php
+        if (!isset($_POST['submit'])){
+          echo 'not clicked';
+          mysqli_close($con);
+        }else{
+          echo 'button clicked' . '<br />';
+          $iname = $_POST['iname'];
+          $iphone = $_POST['iphone'];
+          $qur = "INSERT INTO tb_con (SN,Name,Contact) VALUES('','$iname','$iphone')";
+
+          $result = mysqli_query($con,$qur);
+
+          if ($result) {
+            echo "inserted to database";
+          }else{
+            echo "cannot insert to database";
+          }
+          echo '<br />' . 'display data of database' . '<br />';
+          $query = 'SELECT * from tb_con;';
+          $result = mysqli_query($con,$query);
+          $resultcheck = mysqli_num_rows($result);
+          echo $resultcheck . 'rows in the table.' . '<br />';
+          if ($resultcheck > 0) {
+            while($row = mysqli_fetch_assoc($result)){
+              echo $row['SN'] . ' ' . $row['Name']. ' ' . $row['Contact'] . ' ' . '<br />';
+            }
+          }
+          mysqli_free_result($result);
+          mysqli_close($con);
+        }
+      ?>
+
+
+
     </div>
   </body>
 </html>
